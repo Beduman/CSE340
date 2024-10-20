f98f712b2ae4c1262790a3e613762b59dd9747cb
Beduman <behunz77@gmail.com>
2024-10-19 21:53:09 -0700

    error handling

--- routes/static.js
+++ routes/static.js
@@ -8,6 +8,13 @@ router.use("/css", express.static(__dirname + "public/css"));
 router.use("/js", express.static(__dirname + "public/js"));
 router.use("/images", express.static(__dirname + "public/images"));
 
+/* ****************************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for 
+ * General Error Handling
+ **************************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
+
 module.exports = router;
 
 
