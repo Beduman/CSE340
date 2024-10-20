2b9a372366d8526dd128c6198aa46badefa8a26b
Beduman <behunz77@gmail.com>
2024-10-19 21:54:27 -0700

    .

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -9,11 +9,4 @@ router.get("/type/:classificationId", invController.buildByClassificationId);
 //route to build inventory detail view
 router.get("/type/:inventoryId", invController.buildByInventoryId);
 
-/* ****************************************
- * Middleware For Handling Errors
- * Wrap other function in this for 
- * General Error Handling
- **************************************** */
-Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
-
 module.exports = router;
